8efb253594042131f0e56101d585ed60e52b01b5
Lukas Kropatschek <lukas@kropatschek.net>
2016-10-16 13:03:21 +0200

    improvements..

--- manifests/certonly.pp
+++ manifests/certonly.pp
@@ -4,20 +4,22 @@ define certbot::certonly (
   $domains        = [],
   $renew_cron     = Hash[String, Integer, 2],
   $force_renewal  = false,
-  #$cert_path      = undef,
-  #$key_path       = undef,
-  #$chain_path     = undef,
-  #$fullchain_path = undef,
 ) {
   $_domains = join($domains, " -d ")
   $main_domain = $domains[0]
 
-  #$mytemplate = template('certbot/certbot_cmd.erb')
-  #notify { "check $title": message => $mytemplate }
-  exec { "certbot_certonly_webroot $main_domain":
-    path    => '/usr/bin',
-    command => template('certbot/certbot_cmd.erb'),
-    creates => "/etc/letsencrypt/renewal/$main_domain.conf",
+
+  if ($force_renewal) {
+    exec { "certbot_certonly_webroot $main_domain":
+      path    => '/usr/bin',
+      command => template('certbot/certbot_cmd.erb'),
+    }
+  } else {
+    exec { "certbot_certonly_webroot $main_domain":
+      path    => '/usr/bin',
+      command => template('certbot/certbot_cmd.erb'),
+      creates => "/etc/letsencrypt/renewal/$main_domain.conf",
+    }
   }
 
   if is_hash($renew_cron) {
@@ -26,7 +28,7 @@ define certbot::certonly (
         ensure  => present,
         hour    => $renew_cron[hour],
         minute  => $renew_cron[minute],
-        command => "/usr/bin/certbot renew -q -t --agree-tos",
+        command => '/usr/bin/certbot renew -q -t --agree-tos --post-hook "systemctl restart nginx.service"',
       }
     }
   } else {
